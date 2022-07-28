316e647234f3268de5306f85ba1a32bf99556cc9
jiangquan <1401330015@qq.com>
2022-07-29 01:01:52 +0800

    修改数组队列的demo

--- 02-Stacks-and-Queues/src/com/jq/queue/ArrayQueueTestDemo.java
+++ 02-Stacks-and-Queues/src/com/jq/queue/ArrayQueueTestDemo.java
@@ -1,17 +1,18 @@
 package com.jq.queue;
 
-public class QueueTestDemo {
+public class ArrayQueueTestDemo {
     public static void main(String[] args) {
 
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
         for (int i = 0; i < 10; i++) {
             arrayQueue.enqueue(i);
             System.out.println(arrayQueue);
-        }
 
-        for (int i = arrayQueue.getSize(); i > 0; i--) {
-            arrayQueue.dequeue();
-            System.out.println(arrayQueue);
+            // 每入队三个元素，出队一个元素
+            if(i % 3 == 2){
+                arrayQueue.dequeue();
+                System.out.println(arrayQueue);
+            }
         }
     }
 }
